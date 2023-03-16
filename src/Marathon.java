c86ba4c3654150b06a7e5282d0659c479c940dea
wangdm <wangdm08@hotmail.com>
2023-03-16 19:55:27 +0800

    Assignment 3

---
+++ src/Marathon.java
@@ -0,0 +1,38 @@
+class Marathon {
+    public static int fastest (String[] name, int[] time) {
+        int first = 0;
+        for (int i = 1; i < name.length; i++) {
+            if (time[i]<time[first]) {
+                first=i;
+            }
+        }
+        return first;
+    }
+    public static int secondFastest (String[] secondName, int[] secondTime, int first) {
+        int second = 0;
+        for (int j = 0; j < secondName.length; j++) {
+            if (j == first) {
+                continue;
+            }
+            if (secondTime[j] < secondTime[second]) {
+                second = j;
+            }
+        }
+        return second;
+    }
+    public static void main (String[] arguments) {
+        String[] names = {
+                "Elena", "Thomas", "Hamilton", "Suzie", "Phil", "Matt", "Alex",
+                "Emma", "John", "James", "Jane", "Emily", "Daniel", "Neda",
+                "Aaron", "Kate"
+        };
+        int[] times = {
+                341, 273, 278, 329, 445, 402, 388, 275, 243, 334, 412, 393, 299,
+                343, 317, 265
+        };
+        int firstNum = fastest(names, times);
+        int secondNum = secondFastest(names,times,firstNum);
+        System.out.println("Fastest runner is " + names[firstNum] + ": " + times[firstNum]);
+        System.out.println("Second fastest runner is " + names[secondNum] + ": " + times[secondNum]);
+    }
+}
