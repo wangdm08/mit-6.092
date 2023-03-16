b07cb93fef2c14d9b11f65f4ef3997dcc2abaf00
wangdm <wangdm08@hotmail.com>
2023-03-16 17:02:34 +0800

    Initial commit

---
+++ src/FooCorporation.java
@@ -0,0 +1,26 @@
+class FooCorporation{
+    public static void totalPay(double base, int hours){
+            double pay;
+            if (base<8.00){
+                System.out.println("base pay does not meet minimum,error");
+            }
+            else {
+                if (hours<=40) {
+                    pay = base*hours;
+                    System.out.println("Total pay is" + pay);
+                }
+                else if (hours>40 && hours<=60){
+                    pay = base*40+(hours-40)*1.5*base;
+                    System.out.println("Total pay is" + " " + pay);
+                }
+                else {
+                    System.out.println("hours worked exceed the maximum,error");
+                }
+            }
+    }
+    public static void main(String[] args){
+        totalPay(7.50,35);
+        totalPay(8.20,47);
+        totalPay(10.00,73);
+    }
+}
