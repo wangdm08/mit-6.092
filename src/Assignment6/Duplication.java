ec0c10683f68f95f170eda24e22239f683c59859
wangdm <wangdm08@hotmail.com>
2023-04-21 17:03:14 +0800

    assignment 6

--- src/Assignment6/Duplication.java
+++ src/Assignment6/Duplication.java
@@ -2,13 +2,13 @@ package Assignment6;
 
 import java.awt.Graphics;
 
-public class ReduceDuplication {
+public class Duplication {
     private int x;
     private int y;
     private int xDirection;
     private int yDirection;
     private Sprite sprite;
-    public ReduceDuplication(int x, int y, int xDirection, int yDirection, Graphics surface) {
+    public Duplication(int x, int y, int xDirection, int yDirection, Graphics surface) {
         x += xDirection;
         y += yDirection;
 
